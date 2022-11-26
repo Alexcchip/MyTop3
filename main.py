ea22794de16cc36f39d63eda7658342a7929d088
Alex W <alexcchip@gmail.com>
2022-11-26 15:42:55 -0500

    .

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    pass
+    return render_template('index.html')
 
 @app.route('/list')
 def list():
-    pass
+    return render_template('list.html')
 
 if __name__ == 'main':
     app.run(debug=True)

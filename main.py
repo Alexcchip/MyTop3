27e740314d47a3dd02813ca77a20c63ee557eaa7
Alex W <alexcchip@gmail.com>
2022-11-26 15:36:44 -0500

    .

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     pass
 
+@app.route('/list')
+def list():
+    pass
+
+if name == 'main':
+    app.run(debug=True)

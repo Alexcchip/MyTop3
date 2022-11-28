12875c6be843b606c4d9e8a2a45fec5a11b5ce40
Alex W <alexcchip@gmail.com>
2022-11-27 20:09:48 -0500

    Matching system??

--- matching.py
+++ matching.py
@@ -1,11 +1,10 @@
 from main import *
 
 numOfIds = len(User.query.order_by(User.username).all())
-
+bands = dict()
 for i in range(numOfIds+1):
-    #print(i)
     if i > 0:
         x = User.query.filter_by(id=i).first()
-        print("Username: " + x.username  +" Band: " + x.band1)
-        #print(x.band1)
-
+        bandList = [x.band1,x.band2,x.band3]
+        bands[x.username] = bandList
+print(bands)

1511352a9388abdf7ab44ffaec4fd747bf4dc635
Sudiptto Biswas <biswassudiptto@gmail.com>
2022-11-28 12:39:39 -0500

    s

--- matching.py
+++ matching.py
@@ -1,11 +1,1 @@
 from main import *
-
-numOfIds = len(User.query.order_by(User.username).all())
-
-for i in range(numOfIds+1):
-    #print(i)
-    if i > 0:
-        x = User.query.filter_by(id=i).first()
-        print("Username: " + x.username  +" Band: " + x.band1)
-        #print(x.band1)
-
